3850f7bb04224de734b0e6b6f69f1cc3bfd76f90
felipegit@outlook.com <felipegit@outlook.com>
2025-05-08 22:53:01 -0500

    Modelos y configuración del DbContext

--- Program.cs
+++ Program.cs
@@ -9,32 +9,25 @@ namespace SupermarketWEB
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add services to the container.  
             builder.Services.AddRazorPages();
+            builder.Services.AddDbContext<SupermarketContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SupermarketDB")));
 
             var app = builder.Build();
 
-            builder.Services.AddDbContext<SupermarketContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("SupermarketDB"))
-            );
-
-            // Configure the HTTP request pipeline.
+            // Configure the HTTP request pipeline.  
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.MapRazorPages();
-
             app.Run();
         }
     }
