ac3f2a51bcf915d73e1f3a30ee9fb2103b384aa5
felipegit@outlook.com <felipegit@outlook.com>
2025-05-09 01:04:05 -0500

    Creacion de layout categories

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -8,7 +8,6 @@ namespace SupermarketWEB.Pages.Categories
 {
     public class IndexModel : PageModel
     {
-
         private readonly SupermarketContext _context;
         public IndexModel(SupermarketContext context)
         {
@@ -16,6 +15,7 @@ namespace SupermarketWEB.Pages.Categories
         }
 
         public IList<Category> Categories { get; set; } = default!;
+
         public async Task OnGetAsync()
         {
             if (_context.Categories != null)
